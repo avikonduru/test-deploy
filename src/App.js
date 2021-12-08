060c1b58d278b83a4fb188de8818cb7cd530cb5a
Avi Konduru <avikonduru@gmail.com>
2021-12-07 23:51:59 -0800

    test

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-} from 'react-router-dom';
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Routes,
+// } from 'react-router-dom';
 
-// css
-import './App.css';
+// // css
+// import './App.css';
 
-// Pages
-// import SettingsPage from './pages/SettingsPage';
-// import KycPage from './pages/KycPage';
-// import InsightsPage from './pages/InsightsPage';
-// import TransactionsPage from './pages/TransactionsPage';
-// import SendMoneyPage from './pages/SendMoneyPage';
-// import CardsPage from './pages/CardsPage';
-// import AccountsPage from './pages/AccountsPage';
-// import GetCashPage from './pages/GetCashPage';
-// import TransactionPage from './pages/TransactionPage';
+// // Pages
+// // import SettingsPage from './pages/SettingsPage';
+// // import KycPage from './pages/KycPage';
+// // import InsightsPage from './pages/InsightsPage';
+// // import TransactionsPage from './pages/TransactionsPage';
+// // import SendMoneyPage from './pages/SendMoneyPage';
+// // import CardsPage from './pages/CardsPage';
+// // import AccountsPage from './pages/AccountsPage';
+// // import GetCashPage from './pages/GetCashPage';
+// // import TransactionPage from './pages/TransactionPage';
 
-// Components
-import Sidebar from './components/Sidebar';
-import Footer from './components/Footer';
+// // Components
+// // import Sidebar from './components/Sidebar';
+// // import Footer from './components/Footer';
 
-// antd
-import { Layout } from 'antd';
+// // antd
+// import { Layout } from 'antd';
 
-const { Content } = Layout;
+// const { Content } = Layout;
 
 function App() {
   return <div>Hello World</div>;
