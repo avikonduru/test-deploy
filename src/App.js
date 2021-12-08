d4d8950fdd1ddc6431fb8e3d47510b953f3f7e0f
Avi Konduru <avikonduru@gmail.com>
2021-12-07 23:47:54 -0800

    test

--- src/App.js
+++ src/App.js
@@ -9,15 +9,15 @@ import {
 import './App.css';
 
 // Pages
-import SettingsPage from './pages/SettingsPage';
-import KycPage from './pages/KycPage';
-import InsightsPage from './pages/InsightsPage';
-import TransactionsPage from './pages/TransactionsPage';
-import SendMoneyPage from './pages/SendMoneyPage';
-import CardsPage from './pages/CardsPage';
-import AccountsPage from './pages/AccountsPage';
-import GetCashPage from './pages/GetCashPage';
-import TransactionPage from './pages/TransactionPage';
+// import SettingsPage from './pages/SettingsPage';
+// import KycPage from './pages/KycPage';
+// import InsightsPage from './pages/InsightsPage';
+// import TransactionsPage from './pages/TransactionsPage';
+// import SendMoneyPage from './pages/SendMoneyPage';
+// import CardsPage from './pages/CardsPage';
+// import AccountsPage from './pages/AccountsPage';
+// import GetCashPage from './pages/GetCashPage';
+// import TransactionPage from './pages/TransactionPage';
 
 // Components
 import Sidebar from './components/Sidebar';
